a22910c666d54831b0adf746b62aad22299a1428
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-10-20 10:18:13 +0530

    add add route and get route for diamond

---
+++ src/app/api/diamond/add/route.ts
@@ -0,0 +1,36 @@
+import Diamond from "@/models/Diamond";
+import connectDB from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  await connectDB();
+  const { amount, price, bonus } = await request.json();
+  if (!amount || !price) {
+    return NextResponse.json(
+      { message: "All fields are required" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    const newDiamond = new Diamond({
+      amount,
+      price,
+      bonus,
+    });
+
+    await newDiamond.save();
+
+    return NextResponse.json(
+      { message: "Diamond entry added Successfully", data: newDiamond },
+      { status: 201 }
+    );
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An error occurred";
+    return NextResponse.json(
+      { message: "Failed to add diamond entry", error: errorMessage },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/diamond/get/route.ts
@@ -0,0 +1,30 @@
+import Diamond from "@/models/Diamond";
+import connectDB from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectDB();
+
+  try {
+    const diamonds = await Diamond.find();
+    if (diamonds.length === 0) {
+      return NextResponse.json(
+        { message: "No diamonds stock found", data: [] },
+        { status: 200 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "Diamonds retrieved successfully", data: diamonds },
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An error occurred";
+    console.error("Failed to retrieve diamonds:", errorMessage);
+    return NextResponse.json(
+      { message: "Failed to retrieve diamonds", error: errorMessage },
+      { status: 500 }
+    );
+  }
+}
