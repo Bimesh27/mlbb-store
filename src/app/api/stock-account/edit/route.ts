6f1351d723f3995915d7ef4cd351462584631733
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-10-22 18:32:21 +0530

    add edit and get route for stock account

---
+++ src/app/api/stock-account/edit/route.ts
@@ -0,0 +1,64 @@
+import MlAccount from "@/models/MlAccount";
+import { checkAdmin } from "@/utils/checkAdmin";
+import connectDB from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function PUT(request: Request) {
+  await connectDB();
+
+  const isAdmin = await checkAdmin();
+  if (!isAdmin) {
+    return NextResponse.json(
+      { message: "Unauthorized to edit stock account" },
+      { status: 403 }
+    );
+  }
+
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+
+    if (!id) {
+      return new Response("id is required", { status: 400 });
+    }
+
+    const { price, description } = await request.json();
+    if (!price && !description) {
+      return NextResponse.json(
+        {
+          message:
+            "At least one field (price or description) is required for update",
+        },
+        { status: 400 }
+      );
+    }
+
+    const stockAccount = await MlAccount.findById(id);
+    if (!stockAccount) {
+      return NextResponse.json(
+        { message: "Stock account not found" },
+        { status: 404 }
+      );
+    }
+
+    if (price) stockAccount.price = price;
+    if (description) stockAccount.description = description;
+
+    await stockAccount.save();
+
+    return NextResponse.json(
+      {
+        message: "Stock account updated successfully",
+        stockAccount: stockAccount,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An error occurred";
+    return NextResponse.json({
+      status: 500,
+      message: errorMessage,
+    });
+  }
+}
---
+++ src/app/api/stock-account/get/route.ts
@@ -0,0 +1,34 @@
+import MlAccount from "@/models/MlAccount";
+import { authencateUser } from "@/utils/checkAuth";
+import connectDB from "@/utils/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  connectDB();
+
+  const {user, error} = await authencateUser();
+  if (!user) {
+    return NextResponse.json({message: error}, {status: 401});
+  }
+  try {
+    const stockAccounts = await MlAccount.find();
+    if (!stockAccounts || stockAccounts.length === 0) {
+      return NextResponse.json({
+        status: 404,
+        message: "No stock accounts found",
+      });
+    }
+    return NextResponse.json({
+      status: 200,
+      message: "Stock accounts found",
+      data: stockAccounts,
+    });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "An error occurred";
+    return NextResponse.json({
+      status: 500,
+      message: errorMessage,
+    });
+  }
+}
