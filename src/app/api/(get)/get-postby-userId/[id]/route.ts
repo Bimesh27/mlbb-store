8fd23bbe91c79aac2acbeaaacc189f08f0aa793e
Bimesh wangkheimayum <bimeshwangkheimayum@gmail.com>
2024-11-07 08:38:52 +0530

    add waait

--- src/app/api/(get)/get-postby-userId/[id]/route.ts
+++ src/app/api/(get)/get-postby-userId/[id]/route.ts
@@ -3,16 +3,20 @@ import connectDB from "@/utils/db";
 import { NextResponse } from "next/server";
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
-  connectDB();
+  // Ensure database connection
+  await connectDB();
   try {
     const { id } = params;
-    const posts = await UserPost.find({createdBy:id}).populate('createdBy');
+    // Fetch posts created by the user and populate the createdBy field
+    const posts = await UserPost.find({ createdBy: id }).populate("createdBy");
+
     if (posts.length === 0) {
       return NextResponse.json(
         { message: "No posts found", success: false },
         { status: 404 }
       );
     }
+
     return NextResponse.json(
       {
         message: "Posts found",
